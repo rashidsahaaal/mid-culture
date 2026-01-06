896a08774e07e68aaffb77bc447d643c0053f92b
sahal <rashidsahal222@gmail.com>
2026-01-06 15:03:49 +0530

    temp

--- project/settings.py
+++ project/settings.py
@@ -147,3 +147,20 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # DEFAULT PRIMARY KEY
 # --------------------------------------------------
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+
+if os.environ.get("RENDER") == "true":
+    try:
+        from django.contrib.auth import get_user_model
+        User = get_user_model()
+
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser(
+                username="admin",
+                email="admin@example.com",
+                password="admin123"
+            )
+    except Exception as e:
+        print("Superuser creation skipped:", e)
+
